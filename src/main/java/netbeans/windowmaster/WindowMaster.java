8599c4d937915d10c8cbbe74c897d995fe272a2b
rbamarc <rbamarc929@gmail.com>
2023-02-02 06:25:10 -0800

    adding inputs for user

--- src/main/java/netbeans/windowmaster/WindowMaster.java
+++ src/main/java/netbeans/windowmaster/WindowMaster.java
@@ -26,6 +26,25 @@ public class WindowMaster {
         float cost;
         float perimeterOfWindow;
         
+        // declare and initialize scanner object to get inputs
+        Scanner myScanner = new Scanner(System.in);
+        
+        //get input from user for the height and width
+        System.out.println("Please enter the height of the window: ");
+        stringHeight = myScanner.nextLine();
+        System.out.println("Please enter the width of the window: ");
+        stringWidth = myScanner.nextLine();
+        //these are string values and need to be converted to float values to 
+        //work with the given data type
+        
+        //convert String values to float values
+        height = Float.parseFloat(stringHeight);
+        width = Float.parseFloat(stringWidth);
+        
+        //currently no error handling to make sure the right kind of values 
+        //were entered, that will be for a later practice
+        
+        
         
     }
 }

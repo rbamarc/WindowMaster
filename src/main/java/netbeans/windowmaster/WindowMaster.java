ea69988e0f051c161a98f950b6852db895cf65e1
rbamarc <rbamarc929@gmail.com>
2023-02-02 06:14:18 -0800

    first commit

---
+++ src/main/java/netbeans/windowmaster/WindowMaster.java
@@ -0,0 +1,31 @@
+
+package netbeans.windowmaster;
+import java.util.Scanner;
+
+/**
+ *
+ * @author halfrikan
+ */
+public class WindowMaster {
+
+    public static void main(String[] args) {
+        //a sample program to let a customer enter in the size of a window
+        //and get a cost output for the job
+        
+        //declare variables for height and width
+        float height;
+        float width;
+        
+        //declare String variables to hold the user's height
+        //and width input
+        String stringHeight;
+        String stringWidth;
+        
+        //declare other variables
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
+        
+        
+    }
+}
